e6d1db5b31b87daca450b65b7f6678728e488ca6
Floozutter <floozutter@gmail.com>
2020-05-08 05:48:15 -0700

    Add coding for a1z26

---
+++ a1z26/README.md
@@ -0,0 +1,2 @@
+# A1Z26
+A simple substitution cipher which uses [1, 26] as a substitution alphabet.
---
+++ a1z26/code.py
@@ -0,0 +1,33 @@
+"""
+A1Z26 encoding/decoding.
+"""
+
+from string import ascii_lowercase
+from typing import Dict
+
+
+letter2number: Dict[str, int] = {
+    letter: i+1 for i, letter in enumerate(ascii_lowercase)
+}
+number2letter: Dict[int, str] = {
+    number: letter for letter, number in letter2number.items()
+}
+
+def encode_unit(unit: str) -> str:
+    """Encodes a single unit of plaintext to ciphertext."""
+    return str(letter2number.get(char.lower(), char))
+
+def decode_unit(unit: str) -> str:
+    """Decodes a single unit of ciphertext to plaintext."""
+    if not unit.isdigit():
+        return unit
+    n = int(unit)
+    return number2letter.get(n, unit)
+
+def encode(plaintext: str, separator: str=" ") -> str:
+    """Encodes plaintext to ciphertext."""
+    return separator.join(map(encode_unit, text))
+
+def decode(ciphertext: str, separator: str=" ") -> str:
+    """Decodes ciphertext to plaintext."""
+    return "".join(map(decode_unit, ciphertext.split(separator)))
