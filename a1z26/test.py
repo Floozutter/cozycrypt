cc3dd95581e541131742ac576eef728f4fa79684
Floozutter <floozutter@gmail.com>
2020-05-08 05:59:16 -0700

    Test that one Phoenix's username in Valorant

--- a1z26/test.py
+++ a1z26/test.py
@@ -7,7 +7,8 @@ import code
 
 
 class TestDecoding(unittest.TestCase):
-    ...
+    def test_steven(self):
+        self.assertEqual(code.decode("19 20 5 22 5 14"), "steven")
 
 
 if __name__ == "__main__":
