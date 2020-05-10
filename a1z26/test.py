7e344cca3e604fa01f7af8569114e85f57e05781
Floozutter <floozutter@gmail.com>
2020-05-10 04:38:39 -0700

    Test the alphabet for encoding and decoding

--- a1z26/test.py
+++ a1z26/test.py
@@ -5,8 +5,18 @@ Tests for A1Z26.
 import unittest
 import code
 
+from string import ascii_lowercase
+
+
+alphabet_numbers = " ".join(map(str, range(1, 27)))
+
+class TestEncoding(unittest.TestCase):
+    def test_alphabet(self):
+        self.assertEqual(alphabet_numbers, code.encode(ascii_lowercase))
 
 class TestDecoding(unittest.TestCase):
+    def test_alphabet(self):
+        self.assertEqual(ascii_lowercase, code.decode(alphabet_numbers))
     def test_steven(self):
         self.assertEqual(code.decode("19 20 5 22 5 14"), "steven")
 
