1f0751e62f5ee914929ca7203ae5d9dbdfdd1fe6
Floozutter <floozutter@gmail.com>
2020-05-10 04:40:48 -0700

    Define rot13 outside of tests

--- rot/test.py
+++ rot/test.py
@@ -8,14 +8,14 @@ import code
 from functools import partial
 
 
+rot13 = partial(code.rotate, 13)
+
 class TestCoding(unittest.TestCase):
     def test_rot13_chicken(self):
-        rot13 = partial(code.rotate, 13)
         a = "Why did the chicken cross the road? Gb trg gb gur bgure fvqr!"
         b = "Jul qvq gur puvpxra pebff gur ebnq? To get to the other side!"
         self.assertEqual(a, rot13(b))
     def test_rot13_involution(self):
-        rot13 = partial(code.rotate, 13)
         pangram = "The quick brown fox jumps over the lazy dog."
         self.assertEqual(pangram, rot13(rot13(pangram)))
 
